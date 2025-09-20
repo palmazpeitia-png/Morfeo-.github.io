523139b72a5031541b05a4a62cc5b4865aa8306f
Palma_azp <palmazpeitia@gmail.com>
2025-09-20 04:32:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function mostrarCodigo() {
+    const codigo = document.getElementById('codigo-js');
+    if (codigo.style.display === 'none') {
+        codigo.style.display = 'block';
+    } else {
+        codigo.style.display = 'none';
+    }
+}
